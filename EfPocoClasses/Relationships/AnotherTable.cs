f412e423d4f031486ec1462f72540331f1a5695d
Demid Sukhovsky <demid.sukhovsky@willistowerswatson.com>
2020-04-20 13:58:56 +1000

    fix foreignKey2 NullReferenceException; fix typo;

---
+++ EfPocoClasses/Relationships/AnotherTable.cs
@@ -0,0 +1,18 @@
+﻿// =====================================================
+// EfSchemeCompare Project - project to compare EF schema to SQL schema
+// Filename: AnotherTable.cs
+// Date Created: //
+// © Copyright Selective Analytics 2020. All rights reserved
+// =====================================================
+
+using System.Collections.Generic;
+
+namespace EfPocoClasses.Relationships
+{
+    public class AnotherTable
+    {
+        public int AnotherTableId { get; set; }
+        public string MyString { get; set; }
+        public List<DataTop> DataTops { get; set; }
+    }
+}
